6496f17d6dcb7cd924c9a8bda0379e9ba297bb5e
StvnDugz <stvndugz@gmail.com>
2023-12-28 11:05:47 +0000

    :hammer: Update player controller

--- Assets/My Assets/Scripts/PlayerController.cs
+++ Assets/My Assets/Scripts/PlayerController.cs
@@ -4,12 +4,13 @@ using UnityEngine.AI;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private Camera cam;
-    private NavMeshAgent navMeshAgent;
     [SerializeField] private Animator animator;
+    private NavMeshAgent navMeshAgent;
 
     void Start()
     {
         navMeshAgent = GetComponent<NavMeshAgent>();
+        cam = Camera.main;
         animator.GetComponent<Animator>();
     }
 
@@ -22,6 +23,10 @@ public class PlayerController : MonoBehaviour
 
             if (Physics.Raycast(camRay, out hit))
             {
+                //if (hit.collider != null && hit.collider.CompareTag("Ground"))
+                //{
+                    MovePlayer(hit.point);
+                //}
                 Vector3 pointToLook = camRay.GetPoint(hit.distance);
                 Debug.DrawLine(camRay.origin, pointToLook, Color.blue);
                 transform.LookAt(new Vector3(pointToLook.x, transform.position.y, pointToLook.z));
@@ -37,6 +42,13 @@ public class PlayerController : MonoBehaviour
         else
         {
             animator.SetBool("IsWalking", true);
+        }
+    }
+    private void MovePlayer(Vector3 destination)
+    {
+        if (navMeshAgent != null)
+        {
+            navMeshAgent.SetDestination(destination);
         }
     }
 }
